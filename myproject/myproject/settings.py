cfc01dc7fc85d8351c088b2f3aa8b45643bdaf6e
AleksVasilievich <104069090+AleksVasilievich@users.noreply.com>
2024-03-29 22:27:10 +0300

    mom .gitignor settigs add requerements.txt

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -20,15 +20,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-vvi_3*)60$15qq#w3b3tfi!bffatpoyd8a3(m=1sppi31m6vw#"
-
+# SECRET_KEY = "django-insecure-vvi_3*)60$15qq#w3b3tfi!bffatpoyd8a3(m=1sppi31m6vw#"
+SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
+    "sprig.pythonanywhere.com",
 ]
 
+INTERNAL_IPS =[
+    "127.0.0.1",
+]
 
 # Application definition
 
@@ -79,13 +83,26 @@ WSGI_APPLICATION = "myproject.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sprig$default',
+        'USER': 'sprig',
+        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+        'HOST': 'sprig.mysql.pythonanywhere-services.com',
+        'OPTIONS': {
+                        'init_command': "SET NAMES 'utf8mb4';SET sql_mode='STRICT_TRANS_TABLES'",
+                        'charset': 'utf8mb4',
+        },
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
